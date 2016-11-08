bb3f005314915e468ed7e0cc9a86f6abc67bf966
guillaumeBoulic <boulic.guillaume@gmail.com>
2016-11-08 14:32:53 +0100

    add of a view holder

---
+++ app/src/main/java/albumViewHolder.java
@@ -0,0 +1,12 @@
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by kriss on 08/11/16.
+ */
+
+public class albumViewHolder {
+    public TextView artiste;
+    public TextView album;
+    public ImageView img;
+}
