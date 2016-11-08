a178ba766c306a2d9db7fd2c4c9f01cde99a46ef
guillaumeBoulic <boulic.guillaume@gmail.com>
2016-11-08 11:48:35 +0100

    start exo4

--- app/src/main/java/com/example/kriss/exo4/Exo4.java
+++ app/src/main/java/com/example/kriss/exo4/Exo4.java
@@ -2,12 +2,21 @@ package com.example.kriss.exo4;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class Exo4 extends AppCompatActivity {
 
+    ListView listView;
+    String[] artiste =new String[] { "David Guetta", "Nirvana", "Robin Des Bois", "Sean Paul"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_exo4);
+
+        listView = (ListView) findViewById(R.id.listView);
+
+        ArrayAdapter<String> adaptateur = new ArrayAdapter<String>()
+
     }
 }
